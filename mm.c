26fc2126b56857aec4dfce434ed8919eb76b7e7a
HTH016 <hthth01116@gmail.com>
2024-02-22 00:17:29 +0900

    mm

--- mm.c
+++ mm.c
@@ -1,6 +1,6 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -18,10 +18,10 @@
 #include "mm.h"
 #include "memlib.h"
 
-/*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
- ********************************************************/
+ /*********************************************************
+  * NOTE TO STUDENTS: Before you do anything else, please
+  * provide your team information in the following struct.
+  ********************************************************/
 team_t team = {
     /* Team name */
     "ateam",
@@ -76,35 +76,38 @@ static void* coalesce(void* bp);
 static void* find_fit(size_t asize);
 static void place(void* bp, size_t asize);
 static char* heap_listp;
+static char* recent_bp;
 
-/* 
+/*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
     /* Creat the initial empty heap */
-    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void*) - 1) {
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void*)-1) {
         return -1;
     }
 
-    PUT(heap_listp, 0);                  
+    PUT(heap_listp, 0);
     PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
     PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
     PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
     heap_listp += (2 * WSIZE);
-    
+
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL) {
         return -1;
     }
+
+    recent_bp = heap_listp;
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
-void *mm_malloc(size_t size)
+void* mm_malloc(size_t size)
 {
     size_t asize;
     size_t extendsize;
@@ -114,7 +117,7 @@ void *mm_malloc(size_t size)
     if (size == 0) {
         return NULL;
     }
-    
+
     /* Adjust block size to include overhead and alignment reqs. */
     if (size <= DSIZE) {
         asize = 2 * DSIZE;
@@ -126,22 +129,24 @@ void *mm_malloc(size_t size)
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {
         place(bp, asize);
+        recent_bp = bp;
         return bp;
     }
 
     /* No fit found. Get more memory and place the block */
     extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL) {
-        return NULL;    
-        }
+        return NULL;
+    }
     place(bp, asize);
+    recent_bp = bp;
     return bp;
 
     /*
     int newsize = ALIGN(size + SIZE_T_SIZE);
     void *p = mem_sbrk(newsize);
     if (p == (void *)-1)
-	return NULL;
+    return NULL;
     else {
         *(size_t *)p = size;
         return (void *)((char *)p + SIZE_T_SIZE);
@@ -153,7 +158,7 @@ void *mm_malloc(size_t size)
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *bp)
+void mm_free(void* bp)
 {
     size_t size = GET_SIZE(HDRP(bp));
 
@@ -165,25 +170,25 @@ void mm_free(void *bp)
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
-void *mm_realloc(void *ptr, size_t size)
+void* mm_realloc(void* ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
+    void* oldptr = ptr;
+    void* newptr;
     size_t copySize;
-    
+
     newptr = mm_malloc(size);
     if (newptr == NULL)
-      return NULL;
-    copySize = (size_t) GET_SIZE(HDRP(oldptr));
+        return NULL;
+    copySize = (size_t)GET_SIZE(HDRP(oldptr));
     if (size < copySize)
-      copySize = size;
+        copySize = size;
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
 }
 
 
-static void* extend_heap(size_t words) 
+static void* extend_heap(size_t words)
 {
     char* bp;
     size_t size;
@@ -207,11 +212,12 @@ static void* extend_heap(size_t words)
 
 static void* coalesce(void* bp)
 {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp))); 
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
     if (prev_alloc && next_alloc) {
+        recent_bp = bp;
         return bp;
     }
 
@@ -235,26 +241,34 @@ static void* coalesce(void* bp)
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
+    recent_bp = bp;
     return bp;
 }
 
 
-static void* find_fit(size_t asize) 
+static void* find_fit(size_t asize)
 {
-    /* First-fit search */
-    void* bp;
-    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+   
+    /* Next-fit search */
+    char* bp = recent_bp;
+    for (bp = recent_bp; (GET_SIZE(HDRP(bp)) > 0); bp = NEXT_BLKP(bp)) {
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
         {
+            recent_bp = bp;
             return bp;
         }
     }
 
-    /* Next-fit search */
-
-    /* Best-fit search */
+    for (bp = heap_listp; bp < recent_bp; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
+        {
+            recent_bp = bp;
+            return bp;
+        }
+    }
 
     return NULL; /* No fit */
+
 //#endif
 }
 
@@ -276,3 +290,23 @@ static void place(void* bp, size_t asize)
 
 }
 
+
+
+/*
+Results for mm malloc:
+trace  valid  util     ops      secs  Kops
+ 0       yes   90%    5694  0.002024  2814
+ 1       yes   93%    5848  0.001289  4537
+ 2       yes   94%    6648  0.003875  1716
+ 3       yes   96%    5380  0.003972  1355
+ 4       yes   66%   14400  0.000152 94550
+ 5       yes   89%    4800  0.004848   990
+ 6       yes   87%    4800  0.004579  1048
+ 7       yes   55%   12000  0.027561   435
+ 8       yes   51%   24000  0.011124  2157
+ 9       yes   26%   14401  0.128683   112
+10       yes   34%   14401  0.003579  4023
+Total          71%  112372  0.191685   586
+
+Perf index = 43 (util) + 39 (thru) = 82/100
+*/
